b07b77db0ca00eebde9a0e44ee9540ceb5c353ad
Yifan Mai <yifan@cs.stanford.edu>
2025-10-20 14:36:50 -0700

    Fixes

--- scripts/validate_data.py
+++ scripts/validate_data.py
@@ -7,7 +7,6 @@
 
 import argparse
 import json
-import logging
 import os
 from typing import Any, Dict
 
@@ -19,16 +18,9 @@ def get_schema(file_path: str) -> Dict[str, Any]:
         return json.load(f)
 
 
-def validate_file(file_path: str) -> None:
+def validate_file(file_path: str, schema: Dict[str, Any]) -> None:
     with open(file_path, "r") as f:
         instance = json.load(f)
-    schema = {
-        "type" : "object",
-        "properties" : {
-            "price" : {"type" : "number"},
-            "name" : {"type" : "string"},
-        },
-    }
     jsonschema.validate(instance, schema)
 
 
@@ -46,28 +38,43 @@ def expand_paths(paths: str) -> str:
         else:
             raise Exception(f"Could not find file or directory at path: {path}")
     return file_paths
-    
+
 
 def main():
     parser = argparse.ArgumentParser(
         prog="validate_data",
         description="Validates that the JSON data conforms to the Pydantic schema",
     )
-    parser.add_argument('paths', nargs="+", type=str, help="File or folder paths to the JSON data")
-    parser.add_argument('-s', '--schema-path', type=str, help="File path to the JSON schema", required=True)
+    parser.add_argument(
+        "paths", nargs="+", type=str, help="File or folder paths to the JSON data"
+    )
+    parser.add_argument(
+        "-s",
+        "--schema-path",
+        type=str,
+        help="File path to the JSON schema",
+        required=True,
+    )
     args = parser.parse_args()
     file_paths = expand_paths(args.paths)
-    has_errors = False
+    num_passed = 0
+    num_failed = 0
     schema = get_schema(args.schema_path)
+    print()
+    print(f"Validating {len(file_paths)} JSON files...")
+    print()
     for file_path in file_paths:
         try:
-            validate_file(file_path)
+            validate_file(file_path, schema)
+            num_passed += 1
         except Exception as e:
-            has_errors = True
-            print(str(e))
-    if has_errors:
+            print(f"In {file_path}:")
+            print(f"  {type(e).__name__}: {str(e)}")
+            print()
+            num_failed += 1
+    print(f"{num_passed} file(s) passed; {num_failed} file(s) failed")
+    if num_failed > 0:
         exit(1)
-    
 
 
 if __name__ == "__main__":
